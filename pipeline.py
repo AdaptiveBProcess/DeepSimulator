9f991bd3420a01933accace748c47d0b008d0d4d
Mcamargo85 <manuel.camargo@gmail.com>
2022-09-06 18:00:57 +0300

    feature: environment update

--- pipeline.py
+++ pipeline.py
@@ -4,23 +4,25 @@ Created on Fri Jun 26 13:27:58 2020
 
 @author: Manuel Camargo
 """
-import os
-import sys
 import getopt
 import multiprocessing
+import os
+import sys
 
 import deep_simulator as ds
 
+
 def catch_parameter(opt):
     """Change the captured parameters names"""
-    switch = {'-h': 'help', '-f': 'file', '-g': 'update_gen', 
-              '-i': 'update_ia_gen', '-p': 'update_mpdf_gen', 
-              '-t': 'update_times_gen', '-s': 'save_models', 
+    switch = {'-h': 'help', '-f': 'file', '-g': 'update_gen',
+              '-i': 'update_ia_gen', '-p': 'update_mpdf_gen',
+              '-t': 'update_times_gen', '-s': 'save_models',
               '-e': 'evaluate', '-m': 'mining_alg'}
     try:
         return switch[opt]
-    except:
-        raise Exception('Invalid option ' + opt)
+    except Exception:
+        raise ValueError('Invalid option ' + opt)
+
 
 # --setup--
 def main(argv):
@@ -30,21 +32,21 @@ def main(argv):
     # parms setting manual fixed or catched by console
     if not argv:
         # Event-log parms
-        parms['gl']['file'] = 'confidential_2000_modif_upd.xes'
+        parms['gl']['file'] = 'Production.xes'
         parms['gl']['update_gen'] = False
         parms['gl']['update_ia_gen'] = False
-        parms['gl']['update_mpdf_gen'] = False
-        parms['gl']['update_times_gen'] = False
+        parms['gl']['update_mpdf_gen'] = True
+        parms['gl']['update_times_gen'] = True
         parms['gl']['save_models'] = True
         parms['gl']['evaluate'] = True
-        parms['gl']['mining_alg'] = 'sm3'
+        parms['gl']['mining_alg'] = 'sm1'
     else:
         # Catch parms by console
         try:
-            opts, _ = getopt.getopt( argv, "h:f:g:i:p:t:s:e:m:",
-                ['file=','update_gen=', 'update_ia_gen=', 'update_mpdf_gen=', 
-                 'update_times_gen=', 'save_models=', 'evaluate=', 
-                 'mining_alg='])
+            opts, _ = getopt.getopt(argv, "h:f:g:i:p:t:s:e:m:",
+                                    ['file=', 'update_gen=', 'update_ia_gen=', 'update_mpdf_gen=',
+                                     'update_times_gen=', 'save_models=', 'evaluate=',
+                                     'mining_alg='])
             for opt, arg in opts:
                 key = catch_parameter(opt)
                 if arg in ['None', 'none']:
@@ -57,14 +59,14 @@ def main(argv):
         except getopt.GetoptError:
             print('Invalid option')
             sys.exit(2)
-    parms['gl']['sim_metric'] = 'tsd' # Main metric
+    parms['gl']['sim_metric'] = 'tsd'  # Main metric
     # Additional metrics
     parms['gl']['add_metrics'] = ['day_hour_emd', 'log_mae', 'dl', 'mae']
     parms['gl']['exp_reps'] = 1
     # Sequences generator
     parms['s_gen'] = dict()
-    parms['s_gen']['repetitions'] = 5
-    parms['s_gen']['max_eval'] = 30
+    parms['s_gen']['repetitions'] = 1
+    parms['s_gen']['max_eval'] = 2
     parms['s_gen']['concurrency'] = [0.0, 1.0]
     parms['s_gen']['epsilon'] = [0.0, 1.0]
     parms['s_gen']['eta'] = [0.0, 1.0]
@@ -72,29 +74,30 @@ def main(argv):
     parms['s_gen']['gate_management'] = ['discovery', 'equiprobable']
     # Inter arrival generator
     parms['i_gen'] = dict()
-    parms['i_gen']['batch_size'] = 32 # Usually 32/64/128/256
+    parms['i_gen']['batch_size'] = 32  # Usually 32/64/128/256
     parms['i_gen']['epochs'] = 100
-    parms['i_gen']['gen_method'] = 'prophet' # pdf, dl, mul_pdf, test, prophet
+    parms['i_gen']['gen_method'] = 'prophet'  # pdf, dl, mul_pdf, test, prophet
     # Times allocator parameters
     parms['t_gen'] = dict()
     parms['t_gen']['imp'] = 1
     parms['t_gen']['max_eval'] = 12
-    parms['t_gen']['batch_size'] = 32 # Usually 32/64/128/256
+    parms['t_gen']['batch_size'] = 32  # Usually 32/64/128/256
     parms['t_gen']['epochs'] = 200
     parms['t_gen']['n_size'] = [5, 10, 15]
-    parms['t_gen']['l_size'] = [50, 100] 
+    parms['t_gen']['l_size'] = [50, 100]
     parms['t_gen']['lstm_act'] = ['selu', 'tanh']
     parms['t_gen']['dense_act'] = ['linear']
     parms['t_gen']['optim'] = ['Nadam']
-    parms['t_gen']['model_type'] = 'dual_inter' # basic, inter, dual_inter, inter_nt
-    parms['t_gen']['opt_method'] = 'rand_hpc' # bayesian, rand_hpc
-    parms['t_gen']['all_r_pool'] = True # only intercase features
-    parms['t_gen']['reschedule'] = False # reschedule according resource pool ocupation
-    parms['t_gen']['rp_similarity'] = 0.80 # Train models
+    parms['t_gen']['model_type'] = 'dual_inter'  # basic, inter, dual_inter, inter_nt
+    parms['t_gen']['opt_method'] = 'bayesian'  # bayesian, rand_hpc
+    parms['t_gen']['all_r_pool'] = True  # only intercase features
+    parms['t_gen']['reschedule'] = False  # reschedule according resource pool ocupation
+    parms['t_gen']['rp_similarity'] = 0.80  # Train models
     print(parms['gl']['file'])
     print(parms)
     simulator = ds.DeepSimulator(parms)
     simulator.execute_pipeline()
+
 
 def define_general_parms(parms):
     """ Sets the app general parms"""
@@ -105,10 +108,10 @@ def define_general_parms(parms):
     parms['gl'] = dict()
     # Event-log reading options
     parms['gl']['read_options'] = {
-            'timeformat': '%Y-%m-%dT%H:%M:%S.%f',
-            'column_names': column_names,
-            'one_timestamp': False,
-            'filter_d_attrib': True}
+        'timeformat': '%Y-%m-%dT%H:%M:%S.%f',
+        'column_names': column_names,
+        'one_timestamp': False,
+        'filter_d_attrib': True}
     # Folders structure
     parms['gl']['event_logs_path'] = os.path.join('input_files', 'event_logs')
     parms['gl']['bpmn_models'] = os.path.join('input_files', 'bpmn_models')
@@ -118,23 +121,23 @@ def define_general_parms(parms):
     parms['gl']['times_gen_path'] = os.path.join('input_files', 'times_gen_models')
     # External tools routes
     parms['gl']['sm2_path'] = os.path.join('external_tools',
-                                     'splitminer2',
-                                     'sm2.jar')
+                                           'splitminer2',
+                                           'sm2.jar')
     parms['gl']['sm1_path'] = os.path.join('external_tools',
-                                     'splitminer',
-                                     'splitminer.jar')
+                                           'splitminer',
+                                           'splitminer.jar')
     parms['gl']['sm3_path'] = os.path.join('external_tools',
-                                     'splitminer3',
-                                     'bpmtk.jar')
+                                           'splitminer3',
+                                           'bpmtk.jar')
     parms['gl']['bimp_path'] = os.path.join('external_tools',
-                                      'bimp',
-                                      'qbp-simulator-engine.jar')
+                                            'bimp',
+                                            'qbp-simulator-engine.jar')
     parms['gl']['align_path'] = os.path.join('external_tools',
-                                       'proconformance',
-                                       'ProConformance2.jar')
+                                             'proconformance',
+                                             'ProConformance2.jar')
     parms['gl']['calender_path'] = os.path.join('external_tools',
-                                          'calenderimp',
-                                          'CalenderImp.jar')
+                                                'calenderimp',
+                                                'CalenderImp.jar')
     parms['gl']['simulator'] = 'bimp'
     return parms
 
